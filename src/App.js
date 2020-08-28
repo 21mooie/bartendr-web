2c6b9a347310bb6a285aac7b4545efb8a2b35d57
Muata Nkosi <muatankosi1@gmail.com>
2020-08-28 01:09:55 -0400

    Style component by centering on page

--- src/App.js
+++ src/App.js
@@ -7,16 +7,20 @@ function App() {
     console.log('i was clicked');
   }
   return (
+
     <div className="App">
+      <div className="hello">
       <p>Hello this is my cocktail app.</p>
-      <!-- Search bar for unauthed users who only want to search -->
+      {/*Search bar for unauthed users who only want to search*/}
       <Button variant="contained" color="primary" onClick={() => clickMe()}>
         Log in
       </Button>
       <Button variant="contained" color="primary">
         Sign up
       </Button>
+      </div>
     </div>
+
   );
 }
 
