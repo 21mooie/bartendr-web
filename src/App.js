8cabc463973637c40fe44d948afda56f2d1658cd
Muata Nkosi <muatankosi1@gmail.com>
2020-08-29 19:01:57 -0400

    Implement use of history object and usage of react-router-dom for routing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import './App.css';
 import {Home} from "./components/Home/Home";
 import {ConnectedDashboard} from "./components/Dashboard/Dashboard";
 import { store } from './store/index';
+import { Router, Route } from 'react-router-dom';
+import {history} from "./store/history";
+import Login from "./components/Login/Login";
+import {Navigation} from "./components/Navigation/Navigation";
 
 
 
@@ -12,14 +16,18 @@ console.log(store.getState());
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        {/*Search bar for unauthed users who only want to search*/}
-        {/*  Router which switches between components, Guard against auth routes*/}
-        <Home />
-        <ConnectedDashboard />
-      </div>
-    </Provider>
+    <Router history={history}>
+      <Provider store={store}>
+        <div className="App">
+          <Navigation />
+          {/*Search bar for unauthed users who only want to search*/}
+          {/*  Router which switches between components, Guard against auth routes*/}
+          <Route exact path="/" render={() => ( <Home /> ) } />
+          <Route path="/dashboard" render={() => (<ConnectedDashboard />)}/>
+          <Route path="/login" render={() => (<Login />)} />
+        </div>
+      </Provider>
+    </Router>
   );
 }
 
