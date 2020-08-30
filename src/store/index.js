762b4ab5b46d2485512d86f5d05a1da0da95def8
Muata Nkosi <muatankosi1@gmail.com>
2020-08-29 20:33:30 -0400

    Use logger to show actions impacting application state

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { user } from '../server/defaultState.js';
+import {createLogger} from "redux-logger";
+
 
 export const store = createStore(
   function reducer(state = user, action) {
     return state;
-  }
+  },
+  applyMiddleware(createLogger())
 )
