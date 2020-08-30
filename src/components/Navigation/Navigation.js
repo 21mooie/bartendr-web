57e45ae87871a2e95862f1ab71cf6551d43e6ece
Muata Nkosi <muatankosi1@gmail.com>
2020-08-29 22:33:54 -0400

    Use Location from react-router-dom to tell Navigation which route we currently are on

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,16 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Link, useLocation } from "react-router-dom";
 
 const Navigation = () => {
+  const routerLocation = useLocation();
+  const [location, setLocation] = useState(routerLocation.pathname);
+  useEffect(() => {
+     setLocation(routerLocation.pathname);
+  }, [routerLocation.pathname]);
   return <div>
     <Link to="dashboard">
       <h1>My dashboard</h1>
+      <p>{location}</p>
     </Link>
   </div>
 }
