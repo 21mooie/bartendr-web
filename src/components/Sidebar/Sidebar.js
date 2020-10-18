530fb6b7d31b302af9db4f29be7f1228a3132cb5
Muata Nkosi <muatankosi1@gmail.com>
2020-10-18 02:36:37 -0400

    Create and style the elements of Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import CloseIcon from '@material-ui/icons/Close';
+import {Link} from "react-router-dom";
 
 import './Sidebar.css';
 
 function Sidebar({isOpen, triggerCloseSidebar}) {
   return (
     <div className={"Sidebar-Container " + (isOpen ? "viewSidebar zeroTop" : "hideSidebar negativeTop")}>
-      <CloseIcon fontSize="large" onClick={() => {triggerCloseSidebar()}}/>
-      <h1>hello</h1>
+      <div className="cancel">
+        <CloseIcon fontSize="large" onClick={() => {triggerCloseSidebar()}}/>
+      </div>
+      <div className="menu">
+        <Link to="/dashboard">
+          <p>Dashboard</p>
+        </Link>
+        <Link to="/user">
+          <p>User</p>
+        </Link>
+        <Link to="/search">
+          <p>Search</p>
+        </Link>
+        <div>
+          <p>Sign Out</p>
+        </div>
+      </div>
     </div>
   );
 }
