9d250470d62aaf8bf3460bfd49f89ca41358c024
Muata Nkosi <muatankosi1@gmail.com>
2020-11-14 06:53:35 -0500

    Integrate Auth0 login widget into application

--- src/components/features/Dashboard/Dashboard.js
+++ src/components/features/Dashboard/Dashboard.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { connect } from "react-redux";
+import { useAuth0 } from "@auth0/auth0-react";
 
 
-export const Dashboard = ({user}) => {
- return (
-   <div>
-      <h2> Dashboard </h2>
-      Hi {user.username}!
-   </div>
- );
+export const Dashboard = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+   return (
+     isAuthenticated && (
+       <div>
+         <img src={user.picture} alt={user.name} />
+         <h2>{user.name}</h2>
+         <p>{user.email}</p>
+       </div>
+     )
+   );
 }
 
 function mapStateToProps(state) {
