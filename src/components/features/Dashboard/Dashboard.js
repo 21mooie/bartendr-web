51777088194b198cbe30b68eebb420949ba45512
Muata Nkosi <muatankosi1@gmail.com>
2020-11-26 15:07:31 -0500

    Fix app to handle authenticated refresh

--- src/components/features/Dashboard/Dashboard.js
+++ src/components/features/Dashboard/Dashboard.js
@@ -1,34 +1,18 @@
 import React from 'react';
 import { connect } from "react-redux";
-import { useAuth0 } from "@auth0/auth0-react";
-import {Link, Redirect} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 export const Dashboard = ({user}) => {
-  const { isAuthenticated } = useAuth0();
-  function handleUnAuth() {
-    if (!isAuthenticated) {
-      return <Redirect to="/" />;
-    } else {
-      return data()
-    }
-  }
 
-  function data() {
-    return (
-      <div>
-        <h2>{user.username}</h2>
-        <p>{user.email}</p>
-        <Link to="/user/muatatest1125">
-          Click here
-        </Link>
-      </div>
-    )
-  }
    return (
-     <>
-      { handleUnAuth() }
-    </>
+     <div>
+       <h2>{user.username}</h2>
+       <p>{user.email}</p>
+       <Link to="/user/muatatest1125">
+         Click here
+       </Link>
+     </div>
    );
 }
 
