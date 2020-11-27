a3b948fd62282bd426ec9bacd82127942a26ece9
Muata Nkosi <muatankosi1@gmail.com>
2020-11-27 15:29:22 -0500

    Render drink results from api on front end

--- src/components/features/Search/Search.js
+++ src/components/features/Search/Search.js
@@ -5,7 +5,8 @@ import Input from '@material-ui/core/Input';
 import axios from 'axios';
 
 import './Search.css';
-import {url} from "../../../consts/consts";
+import {url} from "../../../consts";
+import DrinkCard from "../../common/DrinkCard/DrinkCard";
 
 
 const useStyles = theme => ({
@@ -19,14 +20,22 @@ class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchVal: ''
+      searchVal: '',
+      drinkResults: {
+        drinks: []
+      },
     };
   };
 
   sendQuery() {
-    axios.post(`${url}/search`, {query: this.state.searchVal})
-      .then((data) => {
+    axios.post(`${url}/query`, {query: this.state.searchVal})
+      .then(({data}) => {
         console.log(data);
+        if (data.drinkResults.drinks) {
+          this.setState({drinkResults: data.drinkResults});
+        } else {
+          this.setState({drinkResults: { drinks: [] }});
+        }
       });
   }
 
@@ -35,7 +44,6 @@ class Search extends Component {
     return (
       <div className='search'>
         <div className='search__container'>
-
           <Input
             className={this.props.classes.input}
             placeholder="Search ..."
@@ -47,6 +55,13 @@ class Search extends Component {
           >
             <SearchIcon />
           </button>
+        </div>
+        <div className='search__resultsContainer'>
+          {
+            this.state.drinkResults.drinks.map(drink => (
+              <DrinkCard key={drink.idDrink} drink={drink}/>
+            ))
+          }
         </div>
       </div>
     )
