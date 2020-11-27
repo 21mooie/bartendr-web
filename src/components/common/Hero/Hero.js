fe4ab7356301425d16239b7f4d0fc4920a1456cf
Muata Nkosi <muatankosi1@gmail.com>
2020-11-27 16:13:04 -0500

    Rename classes to use BEM styling

--- src/components/common/Hero/Hero.js
+++ src/components/common/Hero/Hero.js
@@ -5,19 +5,19 @@ import video from "../../../videos/production ID_4694341.mp4";
 import CTAButton from "../Button/CTAButton";
 import { useAuth0 } from "@auth0/auth0-react";
 
-const Hero = (props) => {
+const Hero = () => {
   const { loginWithRedirect } = useAuth0();
   return (
-    <div className="container">
-      <div className="background">
-        <video className="video" loop autoPlay muted>
+    <div className="hero">
+      <div className="hero__background">
+        <video className="hero__video" loop autoPlay muted>
           <source src={video} type="video/mp4"/>
         </video>
       </div>
-      <div className="content">
+      <div className="hero__content">
         <h1>Order the perfect drink, every time</h1>
         <p>Join a community of drinking enthusiasts and share recipes</p>
-        <div className="btnWrapper">
+        <div className="hero__btnWrapper">
           <CTAButton text="Get started" func={loginWithRedirect} icon={true}/>
         </div>
       </div>
