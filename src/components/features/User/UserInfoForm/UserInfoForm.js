f0fdd7c750537edd214b3d14a86f4f20a6983050
Muata Nkosi <muata.nkosi@prudential.com>
2020-12-18 11:12:17 -0500

    Add spacing

--- src/components/features/User/UserInfoForm/UserInfoForm.js
+++ src/components/features/User/UserInfoForm/UserInfoForm.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from "@material-ui/core/Button";
 import TextField from '@material-ui/core/TextField';
 
-import './UserInfoForm';
+import './UserInfoForm.css';
 
 function UserInfoForm({ viewingCurrentUserProfile, editInfoProp, setEditInfoProp, setUsernameProp}) {
     return (
