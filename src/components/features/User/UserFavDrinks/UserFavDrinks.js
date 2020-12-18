5f7347af083e5e499e0495a374e4c7ee2be76261
Muata Nkosi <muata.nkosi@prudential.com>
2020-12-18 11:18:32 -0500

    UserFavDrink.js added to project This component will render the fav drinks of the profile being viewed. If there are no drinks favorited a graphic will be rendered instead.

---
+++ src/components/features/User/UserFavDrinks/UserFavDrinks.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import LocalBarIcon from '@material-ui/icons/LocalBar';
+
+import './UserFavDrinks.css';
+import DrinkCard from '../../../common/DrinkCard/DrinkCard';
+
+
+function UserFavDrinks({fav_drinks}) {
+    return (
+        <div className="userFavDrinks">
+            {
+                fav_drinks.drinks.length > 0 ? 
+                    <>
+                        <h3>Favorite Drinks</h3>
+                        <div className="user__favDrink__grid">
+                            {
+                                fav_drinks.drinks.map((fav_drink) => (
+                                    <div className="user__favDrink__item" key={fav_drink.idDrink}>
+                                        <DrinkCard drink={fav_drink}/>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </>
+                : 
+                    <div className="userFavDrinks__empty">
+                        <LocalBarIcon style={{fontSize: 60}}/>
+                        <h3>No Favorite Drinks</h3>
+                    </div>
+            }
+        </div>
+    )
+}
+
+export default UserFavDrinks
