9ebe4475f306fa7179299e0dc92fe8db917582ab
Muata Nkosi <muatankosi1@gmail.com>
2020-12-24 02:31:38 -0500

    Create view for full SearchBar.js for mobile devices

--- src/components/features/Navigation/SearchBar/SearchBar.js
+++ src/components/features/Navigation/SearchBar/SearchBar.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Input from "@material-ui/core/Input";
 import Button from "@material-ui/core/Button";
 import SearchIcon from "@material-ui/icons/Search";
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 
 import './SearchBar.css';
 
 function SearchBar({searchVal, setSearchVal, performSearch, show, smallScreen}) {
+  const [showFullSearchBar, setShowFullSearchBar] = useState(false);
   return (
     <>
       {
         show ?
-          <div className="search_bar">
+          <div className={`search_bar ${showFullSearchBar ? 'search_bar__full_flex' : ''}`}>
             {
               smallScreen ?
-                <SearchIcon />
+                <>
+                {
+                  showFullSearchBar ?
+                    <div className="search_bar__small_screen search_bar__full_flex">
+                      <ChevronLeftIcon
+                        onClick={() => setShowFullSearchBar(false)}
+                      />
+                      <SearchBar
+                        searchVal={searchVal}
+                        setSearchVal={setSearchVal}
+                        performSearch={performSearch}
+                        show={true}
+                      />
+                    </div>
+                  :
+                    <SearchIcon
+                      onClick={() => setShowFullSearchBar(true)}
+                    />
+                }
+                </>
                 :
                 <>
                   <Input
