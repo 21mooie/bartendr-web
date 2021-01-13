f011f8af276f80609f763c0620403c3a626d120c
Muata Nkosi <muatankosi1@gmail.com>
2021-01-12 22:33:38 -0500

    Create results tab using responsive design

--- src/components/features/Search/SearchResult/SearchResult.js
+++ src/components/features/Search/SearchResult/SearchResult.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 import './SearchResult.css'
+import FavoriteIcon from "@material-ui/icons/Favorite";
 
 
 function SearchResult({result}) {
@@ -13,6 +14,21 @@ function SearchResult({result}) {
         <div className="searchResult__desc">
           <h3>{result.strDrink}</h3>
           <p>{result.strAlcoholic}</p>
+          <p className="searchResults__ingredient">Made with {result.strIngredient1}</p>
+          <div className="searchResults__instructions">
+            {
+              result.strInstructions.length < 50 ?
+                <p>{result.strInstructions}</p>
+              :
+                <p>{result.strInstructions.substr(0, 45)} ...</p>
+            }
+          </div>
+          <p className="searchResult__numLikes">x Likes</p>
+          <FavoriteIcon
+            style={{ fontSize: 40 }}
+            color='secondary'
+          />
+
         </div>
     </div>
   );
