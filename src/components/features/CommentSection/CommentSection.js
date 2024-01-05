2fb344d37b1614391f1f235f0553a75c65278fdd
Muata Nkosi <muatankosi1@gmail.com>
2024-01-04 20:14:16 -0500

    Create CommentSection component

---
+++ src/components/features/CommentSection/CommentSection.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import './CommentSection.css';
+import {url} from "../../../consts";
+
+const CommentSection = ({idDrink, limit}) => {
+    const [comments, setComments] = useState([]);
+    const [offset, setOffset]   = useState('0');
+    useEffect(() => {
+        axios.get(`${url}/cocktail/${idDrink}/comment`,{
+            params: {
+                offset,
+                limit
+            }
+        })
+            .then((result) => {
+                const data = result.data.data;
+                // console.log(data);
+                setComments([...comments,...data]);
+            })
+            .catch((err) => {
+                console.error(err);
+                // create saga to trigger notification
+                // notification
+            });
+    }, []);
+    return (
+        <div className="commentSection">
+  
+            <ul className="commentSection__comments">
+                { comments.length > 0 && comments.map((comment, index) => (<li key={index}>{comment.content}</li>)) }
+                { comments.length === 0 && <li>There are no comments. You can be the first!</li> }
+            </ul>
+ 
+        </div>
+        
+    );
+}
+ 
+export default CommentSection;
