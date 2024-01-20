a43f9692b76b14ec77e198bd030002741d0d45d5
Muata Nkosi <muatankosi1@gmail.com>
2024-01-20 16:21:01 -0500

    Add like and dislike buttons and like counter; also will render a show replies div if a comment has replies made against it

--- src/components/common/Comment/Comment.spec.js
+++ src/components/common/Comment/Comment.spec.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { render, screen, waitForElement } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import Comment from './Comment';
 
 describe('Comment', () => {
-    test('should render.', () => {
-        const commentData = {
+    let commentData;
+    beforeAll(() => {
+        commentData = {
             "commentId": "65804aea979a92734c307e51",
             "content": "Hi this is the twelfth comment",
             "parentId": null,
@@ -17,25 +20,23 @@ describe('Comment', () => {
             "commenterUsername": "muata01092021",
             "commenterAvi": "https://real-image-url.com"
         };
+    });
+    it('should render.', () => {
         render(<Comment commentData={commentData} />);
         expect(screen.getByRole('img')).toBeInTheDocument();
     });
 
-    test('should show empty avatar when no avi is present.', () => {
-        const commentData = {
-            "commentId": "65804aea979a92734c307e51",
-            "content": "Hi this is the twelfth comment",
-            "parentId": null,
-            "commenterUid": "7c0b82f1-835b-42b0-912d-ba893562f2f4",
-            "idDrink": "178332",
-            "dateTimeCreated": "2023-12-18T13:36:42.051Z",
-            "numLikes": 0,
-            "numDislikes": 0,
-            "hasReplies": false,
-            "commenterUsername": "muata01092021",
-            "commenterAvi": ""
-        };
+    it('should show empty avatar when no avi is present.', () => {
+        commentData.commenterAvi = "";
         const { container } = render(<Comment commentData={commentData} />);
         expect(container.querySelector('.MuiAvatar-root')).toBeInTheDocument();
+    });
+
+    it('should show replies when the button is clicked.', () => {
+        commentData.hasReplies = true;
+        const { container } = render(<Comment commentData={commentData} />);
+        expect(container.querySelector('.comment__showReplies_dropUp')).toBeInTheDocument();
+        userEvent.click(container.querySelector('.comment__showReplies'));
+        expect(container.querySelector('.comment__showReplies_dropDown')).toBeInTheDocument();
     });
 });
