6f3d208a7e3ab75cf0fdfbe4bbe40a3ca623eeb7
Muata Nkosi <muatankosi1@gmail.com>
2024-01-20 16:21:01 -0500

    Add like and dislike buttons and like counter; also will render a show replies div if a comment has replies made against it

--- src/components/common/Comment/Comment.js
+++ src/components/common/Comment/Comment.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Avatar } from '@material-ui/core';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ThumbDownIcon from '@material-ui/icons/ThumbDown';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
 
 import './Comment.css';
 
 const Comment = ({commentData}) => {
+    const [showReplies, setShowReplies] = useState(false);
     return (
         <div className="comment">
             <div className="comment__avi__div">
@@ -26,11 +31,28 @@ const Comment = ({commentData}) => {
                 <h4>{commentData.commenterUsername}</h4>
                 <p className="comment__content">{commentData.content}</p>
                 <p className="comment_datePosted">posted {commentData.dateTimeCreated}</p>
+                <div className="comment__interactions">
+                    <ThumbUpIcon style={{cursor: 'pointer'}}/>
+                    <span className="comment__interactions_likeCount">{commentData.numLikes-commentData.numDislikes}</span>
+                    <ThumbDownIcon style={{cursor: 'pointer'}}/>
+                    <span className="comment__interactions_reply">Reply</span>
+                </div>
+                {
+                    commentData.hasReplies &&
+                    <div className="comment__showReplies" onClick={() => setShowReplies(!showReplies)}>
+                        {
+                            showReplies ?
+                                <ArrowDropDownIcon className="comment__showReplies_dropDown"/>
+                            :
+                                <ArrowDropUpIcon className="comment__showReplies_dropUp"/>
+                        }
+                        <span>Show replies</span>
+                    </div>
+                }
             </div>
-            
         </div>
     );
-
+    // TODO: finish writing logic to increment likes and dislikes and count if a user has already liked something
     // write function to figure out how many minutes/hours/days/months/years a comment was written
 }
  
