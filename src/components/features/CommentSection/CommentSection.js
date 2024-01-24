8224474f6059b9d2137687624c77511045995eeb
Muata Nkosi <muatankosi1@gmail.com>
2024-01-24 06:56:51 -0500

    Refactor CommentList to include a loading HOC This HOC is reusable and can be used for showing to UI for any state that is waiting on async data

--- src/components/features/CommentSection/CommentSection.js
+++ src/components/features/CommentSection/CommentSection.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import './CommentSection.css';
-import {url} from "../../../consts";
 import CommentList from '../../common/CommentList/CommentList';
 
 const CommentSection = ({idDrink}) => {
