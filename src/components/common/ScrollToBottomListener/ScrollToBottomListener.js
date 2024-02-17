c5c0f12751e1d28205fbdb76c03a217c8ada343f
Muata Nkosi <muatankosi1@gmail.com>
2024-02-17 13:49:49 -0500

    Remove ScrollToBottomListener

--- src/components/common/ScrollToBottomListener/ScrollToBottomListener.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useBottomScrollListener } from 'react-bottom-scroll-listener';
-
-const ScrollToBottomListener = ({ bottomReachedCallback }) => {
-    useBottomScrollListener(() => bottomReachedCallback());
-    return (<></>);
-}
- 
-export default ScrollToBottomListener;
