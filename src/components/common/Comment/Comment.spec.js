73a982ba00884a4c18f74332d29d64e3340ecf7e
Muata Nkosi <muatankosi1@gmail.com>
2024-02-17 12:04:01 -0500

    Use IntersectionObserver to achieve infinite scroll

--- src/components/common/Comment/Comment.spec.js
+++ src/components/common/Comment/Comment.spec.js
@@ -3,6 +3,7 @@ import { render, screen, waitForElement } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Comment from './Comment';
+import Reply from '';
 
 describe('Comment', () => {
     let commentData;
@@ -38,5 +39,36 @@ describe('Comment', () => {
         expect(container.querySelector('.comment__showReplies_dropUp')).toBeInTheDocument();
         userEvent.click(container.querySelector('.comment__showReplies'));
         expect(container.querySelector('.comment__showReplies_dropDown')).toBeInTheDocument();
+    });
+
+    describe('when it has replies', () => {
+        beforeAll(() => {
+            commentData.hasReplies = true;
+        });
+
+        it('should show replies when requested.', () => {
+            //TODO: Change to reply component mock once I've made them
+            const { container } = render(<Comment commentData={commentData} />);
+            expect(container.querySelector('.reply')).not.toBeInTheDocument();
+            userEvent.click(container.querySelector('.comment__showReplies'));
+            expect(container.querySelector('.reply')).toBeInTheDocument();
+        });
+
+        it('should hide replies when requested.', () => {
+            const { container } = render(<Comment commentData={commentData} />);
+            expect(container.querySelector('.reply')).not.toBeInTheDocument();
+            userEvent.click(container.querySelector('.comment__showReplies'));
+            expect(container.querySelector('.reply')).toBeInTheDocument();
+            userEvent.click(container.querySelector('.comment__showReplies'));
+            expect(container.querySelector('.reply')).not.toBeInTheDocument();
+        });
+
+        it('should not send a new request for replies once replies are hidden then rerequested.', () => {
+            
+        });
+
+        it('if there are more than the paginated number of replies a show more replies option should be shown.', () => {
+
+        });
     });
 });
--- src/components/common/ScrollToBottomListener/ScrollToBottomListener.spec.js
+++ src/components/common/ScrollToBottomListener/ScrollToBottomListener.spec.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 
-import ScrollListener from './ScrollToBottomListener';
+import ScrollToBottomListener from './ScrollToBottomListener';
 
 describe('ScrollListener ', () => {
     it('should call calback when scrolled to bottom of screen.', () => {
         const mock = jest.fn();
-        render(<ScrollListener bottomReachedCallback={mock}/>);
+        render(<ScrollToBottomListener bottomReachedCallback={mock}/>);
         fireEvent.scroll(window, {target: { scrollY: 10000 }});
         expect(mock).toHaveBeenCalled();
     });
