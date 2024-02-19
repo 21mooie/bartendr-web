c5663d79b50fd0abc3f32f546130061508764db3
Muata Nkosi <muatankosi1@gmail.com>
2024-02-19 15:25:42 -0500

    Style CommentBox.js

--- src/components/common/CommentBox/CommentBox.js
+++ src/components/common/CommentBox/CommentBox.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
-const CommentBox = ({}) => {
-    return (<div>Hello World</div>);
+import './CommentBox.css';
+import CTAButton from '../Button/CTAButton';
+
+
+const CommentBox = ({parentId}) => {
+    return (
+        <div className='commentBox'>
+            <div>
+                <textarea />
+                <div class='commentBox__buttons'>
+                    <CTAButton 
+                        text='Comment'
+                        func={() => console.log('hello world')} className='commentBox__submit'
+                        />
+                    <Button variant='outlined' className="commentBox__cancel">Cancel</Button>
+                </div>
+            </div>
+        </div>
+    );
 }
  
 export default CommentBox;
