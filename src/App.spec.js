4b5fb131a39d77c359d870939cb52a0cf295d6e6
Muata Nkosi <muatankosi1@gmail.com>
2024-11-03 16:59:15 -0500

    Fix test case

--- src/App.spec.js
+++ src/App.spec.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const header = screen.getAllByText("Bartendr");
+  const header = screen.getAllByText("Palace");
   expect(header[0]).toBeInTheDocument();
 });
